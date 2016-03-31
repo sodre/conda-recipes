b13692365d2cabcfb4eec0a3e83f252ad66cc0e3
Patrick Sodré <sodre@sodre.co>
2016-03-31 07:52:39 +0000

    disabled xml2

--- postgres-xl-9.5/build.sh
+++ postgres-xl-9.5/build.sh
@@ -1,32 +1,36 @@
 #!/bin/sh
 
-export CFLAGS="-I$PREFIX/include"
 export LIBS='-ltinfo'
-export LDFLAGS="-L$PREFIX/lib"
 
-bash
 if uname | grep Darwin > /dev/null; then
     # 10.9 appears to have an issue with libxml2; clang/ld isn't respecting
     # the flags above, and it attempts to link with /usr/lib/libxml2.2.dynlib,
     # which results in an error and ./configure time.  @Aaron, have you seen
     # this before?  Can you reproduce the error?
     ./configure --prefix=$PREFIX \
+        --with-includes=$PREFIX/include \
+        --with-libraries=$PREFIX/lib \
         --with-readline        \
         --with-python          \
-        --with-openssl         \
-        --with-libxml          \
-        --with-libxslt
+        --with-openssl         
+#        --with-libxml          \
+#        --with-libxslt
 else
     ./configure --prefix=$PREFIX \
+        --with-includes=$PREFIX/include \
+        --with-libraries=$PREFIX/lib \
+        --enable-thread-safety \
         --with-readline        \
         --with-python          \
-        --with-openssl         \
-        --with-libxml          \
-        --with-libxslt
+        --with-openssl         
+#        --with-libxml          \
+#        --with-libxslt
 fi
 
-make
+make 
 make install
-make install-world
 
-bash
+pushd contrib
+make 
+make install
+popd
